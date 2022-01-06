29054c9366c33ad0801b8ce1cb28e466e061aec3
Chris <inyoka@christoph-volkert.de>
2022-01-06 03:18:36 +0100

    remove not more needed celery comment

--- start_inyoka.sh
+++ start_inyoka.sh
@@ -4,5 +4,3 @@ source ~/.venvs/inyoka/bin/activate
 python manage.py migrate
 python manage.py create_superuser --username admin --password $INYOKA_ADMIN_PASSWORD --email 'admin@localhost' || true
 python manage.py runserver 0.0.0.0:8080
-
-#DJANGO_SETTINGS_MODULE=development_settings celery -A inyoka worker -B -l DEBUG &
