0c497ec06283c23893fb949f22ec6c60489a8173
Chris <inyoka@christoph-volkert.de>
2022-07-24 00:18:30 +0200

    Use timeout for database sessions
    Otherwise after some time without any requests, an internal server error occurs in every worker.
    
     - djangos CONN_MAX_AGE is a positive integer of seconds. It's lower than postgres idle session timeout.
     - for postgres see https://www.postgresql.org/docs/14/runtime-config-client.html#GUC-IDLE-SESSION-TIMEOUT

--- production_settings.py
+++ production_settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         'PASSWORD': '{{ secret "inyoka-postgres-password" }}',
         'HOST': 'postgres',
         'PORT': '5432',
-        'CONN_MAX_AGE': None,
+        'CONN_MAX_AGE': 240,
     }
 }
 
